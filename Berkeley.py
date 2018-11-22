bc153b107b3230a3f5eb92f46bf0c08be4597786
Allan Gustavo Fernandes <allan@romanoff.local>
2018-11-22 17:27:08 -0200

    Keep with errors

--- Berkeley.py
+++ Berkeley.py
@@ -74,16 +74,15 @@ class Configuration:
     def remove_host(self, host):
         self.__hosts.remove(host)
 
-
 def tostamp(time):
     time = time.split(':')
     try:
         hour = int(time[2]) * 3600
         min = int(time[1]) * 60
         sec = int(time[0])
+        return (int(hour) + int(min) + int(sec))
     except Exception:
         pass
-    return int(hour + min + sec)
 
 
 def totime(stamp):
@@ -124,48 +123,73 @@ def main(argv):
         # starting master
         conf.read_hosts('clients.txt')
         hosts = conf.get_hosts()
-
+        count = 0
+        dsum  = 0
         while True:
+            avg = 0
             # wait end show logs
             time.sleep(1)
             msg = raw_input("command_: ")
             for host in hosts:
                 host = host.split(':')
-                masterSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                master_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 try:
                     ip   = host[0]
                     port = host[1]
                 except IndexError:
                     pass
-                #REMOVER
-                print port
-                masterSock.sendto(str(msg), (ip, int(port)))
-                data, addr = masterSock.recvfrom(1024)
-                # REMOVER DO CODIGO
-                print 'answer:'
-                print data
-                x = tostamp(data)
-                print x
-                y = totime(x)
-                print y
+                master_sock.sendto(str(msg), (ip, int(port)))
+                data, addr = master_sock.recvfrom(1024)
+                logger.info(data)
+                master_time  = conf.get_time()
+                slave_time   = data
+                master_stamp = tostamp(master_time)
+                slave_stamp  = tostamp(slave_time)
+                delta = 0
+                try:
+                    delta = master_stamp - slave_stamp
+                except TypeError:
+                    pass
 
-                masterSock.close()
+                if delta < conf.get_delta():
+                    count += 1
+                    dsum += delta
+                master_sock.close()
                 if 'quit' in msg:
                     sys.exit(0)
+            avg = dsum / count
+            for host in hosts:
+                host = host.split(':')
+                master_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                try:
+                    ip = host[0]
+                    port = host[1]
+                except IndexError:
+                    pass
+                master_sock.sendto(str('set_time ' + avg), (ip, int(port)))
+                data, addr = master_sock.recvfrom(1024)
+                logger.info(data)
 
     if (conf.get_mode() in '-s'):
         logger.info(conf.get_ip() + ':' + conf.get_port() + ' MODE: Slave')
         # starting slave
         while True:
-            slaveSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            slaveSock.bind(('0.0.0.0', int(conf.get_port())))
-            data, addr = slaveSock.recvfrom(1024)
+            slave_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            slave_sock.bind(('0.0.0.0', int(conf.get_port())))
+            data, addr = slave_sock.recvfrom(1024)
             if 'quit' in data:
-                slaveSock.close()
+                slave_sock.sendto('quiting', addr)
+                slave_sock.close()
                 sys.exit(0)
             if 'get_time' in data:
-                slaveSock.sendto(conf.get_time(), addr)
-            slaveSock.close()
+                slave_sock.sendto(conf.get_time(), addr)
+                slave_sock.close()
+            if 'set_time' in data:
+                data = data.split(' ')
+                slave_stamp = tostamp(conf.get_time()) + data[1]
+                slave_time = totime(slave_stamp)
+                conf.set_time(slave_time)
+                slave_sock.close()
 
 if __name__ == '__main__':
 
--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ python Berkeley.py -s ip=127.0.0.1 port=7000 time=2:20:15 logfile=clock.log &
 #echo "Slave 3.........Starting"
 #python Berkeley.py -s ip=127.0.0.1 port=7003 time=2:23:19 logfile=clock.log &
 
+#delta is seconds (d)
 echo "Master..........Starting"
-python Berkeley.py -m ip=127.0.0.1 port=6999 time=2:26:55 logfile=clock.log d=300
+python Berkeley.py -m ip=127.0.0.1 port=6999 time=2:26:55 logfile=clock.log d=1200
 
  
 
